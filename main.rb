5f888ed3379e51f421c6561d303398b26b5a9488
Clément <gaugainc@yahoo.com>
2024-08-19 17:19:01 +0200

    Resolved

--- main.rb
+++ main.rb
@@ -1,14 +1,18 @@
 class Test
 
   def substrings(plain_text, dictionary)
+    return_hash = Hash.new(0)
     text_array = plain_text.downcase.split
-    new_array = text_array.map {|word| word.downcase.gsub(/[^a-zA-Z0-9\-]/,"")}
-    puts dictionary.intersection(new_array) 
-    # new_array = text_array.intersection(dictionary)
-    # puts new_array
-  end
-end
+    new_array = text_array.sort.map {|word| word.downcase.gsub(/[^a-zA-Z0-9\-]/,"")}
+    # Diviser la chaîne en mots et compter les occurrences dans l'array
+
+    new_array.each do |word|
+      return_hash[word] = new_array.count(word) if dictionary.include?(word)
+    end
+    return_hash
+  end 
+end 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 test = Test.new
-test.substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts test.substrings("Howdy partner, sit down! partner How's it it it going?", dictionary)
